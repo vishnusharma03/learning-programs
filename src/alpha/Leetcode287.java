f05ff93b7c75245f404e8db306df7f24078bb91c
Vishnu Sharma <vishnusharmauss@gmail.com>
2022-08-04 01:05:14 +0530

    Learning JAVA

---
+++ src/alpha/Leetcode287.java
@@ -0,0 +1,25 @@
+package alpha;
+
+public class Leetcode287 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if (correct < nums.length && nums[correct] != nums[i]) {
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
