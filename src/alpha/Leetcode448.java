87a48e42c2e7fa2604d2e99a437e1984f8a54bf7
Vishnu Sharma <vishnusharmauss@gmail.com>
2022-08-04 00:55:11 +0530

    Learning JAVA

---
+++ src/alpha/Leetcode448.java
@@ -0,0 +1,34 @@
+package alpha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode448 {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+                    if (correct < nums.length && nums[correct] != nums[i]) {
+                    int temp = nums[correct];
+                    nums[correct] = nums[i];
+                    nums[i] = temp;
+                }
+                else {
+                    i++;
+                }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (j  != nums[j] - 1){
+                ans.add(j + 1);
+            }
+        }
+        return ans;
+    }
+}
+
